d9fe04b3db7fb6eea405779ebd6ec5722f3b2603
Haraprasad Evince <haraprasad@evincedev.com>
2024-04-05 03:14:21 +0530

    completed hero section

--- harry_portfolio/src/components/Navbar.jsx
+++ harry_portfolio/src/components/Navbar.jsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import {  menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-transparent`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to='/' className='flex items-center gap-2'
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
           }}>
-          <img src='/download.png' alt="logo" className='w-9 h-9 object-contain' />
+          <img src='/logo.png' alt="logo" className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
             Haraprasad &nbsp;
             <span className='sm:block hidden '>| JavaScript Mastery</span>
@@ -32,7 +32,7 @@ const Navbar = () => {
           ))}
         </ul>
         <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img src={toggle ? close : menu}
+          <img src={toggle ? menu : close}
             alt="menu"
             className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => {
