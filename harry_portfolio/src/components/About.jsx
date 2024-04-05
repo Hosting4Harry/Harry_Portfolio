405768cbecebf5bfd73c2faa23fc9d144532c5f9
Haraprasad Evince <haraprasad@evincedev.com>
2024-04-05 11:24:23 +0530

    Completed about section

--- harry_portfolio/src/components/About.jsx
+++ harry_portfolio/src/components/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/no-unknown-property */
+// import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import {
+  SectionWrapper
+} from '../hoc';
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={title}
+            className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Software developers are the architects of the digital age, wielding the power to craft intricate systems and applications that shape our daily lives. They are the architects of the digital age, weaving together code and creativity to bring ideas to life. With a blend of technical prowess and innovative thinking, software developers navigate complex algorithms and languages to build everything from mobile apps to enterprise solutions. Their role extends beyond mere coding; they must also possess strong problem-solving skills, an eye for detail, and the ability to collaborate effectively with teams. In a world driven by technology, software developers stand at the forefront, continuously pushing the boundaries of what&apos;s possible in the digital realm.
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
