aff67c31c2a3b9ab10979d0111d64f6329e16a22
Haraprasad Evince <haraprasad@evincedev.com>
2024-04-08 13:33:43 +0530

    name change

--- harry_portfolio/src/components/Tech.jsx
+++ harry_portfolio/src/components/Tech.jsx
@@ -8,23 +8,23 @@ import { styles } from "../styles";
 const Tech = () => {
   return (
     <>
-     <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
           Technologies I am familiar with
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Tech.
+          Technologies.
         </h2>
       </motion.div>
-    <div className='mt-5 flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+      <div className='mt-5 flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
     </>
-    
+
   )
 }
 
